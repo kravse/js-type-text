fb8d302cc7a5893291ba0479d4cc3fd50a96bd1f
Jared <jarednkrause@gmail.com>
2020-12-06 18:25:50 -0500

    Fix the .stop() function  and add functionality to set multiple intervals and stop them all

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-var cursorInterval;
-var jsTypeInterval;
-
+var typeWriters = [];
 module.exports = {
   start(config, callback) {
-    var text = config.text || "jsTypeText({text: 'Add some text!'}, function(){})";
+    if (!config.text) return;
     var typedTitle = '';
     var cursor = config.cursor !== false;
-    var jsTypeInterval = setInterval(function () {
-      if (typedTitle.length < text.length) {
-        typedTitle = text.slice(0, typedTitle.length + 1);
+
+    var interval = setInterval(function () {
+      if (typedTitle.length < config.text.length) {
+        typedTitle = config.text.slice(0, typedTitle.length + 1);
         callback(typedTitle + (cursor ? "_" : ""));
-      } else if (typedTitle.length === text.length) {
-        clearInterval(jsTypeInterval);
+      } else if (typedTitle.length === config.text.length) {
+        clearInterval(interval);
         if (cursor !== false && config.cursorBlink !== false) {
           var showCursor = true;
-          cursorInterval = setInterval(function () {
+          typeWriters.push(setInterval(function () {
             showCursor = !showCursor;
             callback(typedTitle + (showCursor ? "_" : ""));
-          }, config.cursorSpeed || 350);
+          }, config.cursorSpeed || 350));
         }
       }
     }, config.speed || 150);
+    typeWriters.push(interval)
   },
   stop() {
-    clearInterval(jsTypeInterval);
-    clearInterval(cursorInterval);
+    for (var i = 0; i < typeWriters.length; i++) {
+      clearInterval(typeWriters[i]);
+    }
   }
 }
